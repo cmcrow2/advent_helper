e8cc89b0166b5bba9619ca631ddc27c6c6beca03
Cam Crow <19ccrow99@gmail.com>
2024-12-13 11:17:20 -0600

    fix: removed commented template

--- prompts/aoc_prompt_template.py
+++ prompts/aoc_prompt_template.py
@@ -2,25 +2,6 @@ from langchain.prompts import PromptTemplate
 
 # Define the prompt template
 aoc_prompt_template = PromptTemplate(
-    # template=(
-    #     "You are an expert assistant designed to help with Advent of Code programming challenges. "
-    #     "You should only answer questions that are directly related to Advent of Code. "
-    #     "For other topics, politely decline to answer.\n\n"
-    #     "You have access to the following tool(s):\n"
-    #     "{tools}\n\n"
-    #     "When deciding whether to use a tool, consider the following:\n"
-    #     "- If the question involves a solution to a specific Advent of Code challenge, use one of the provided tools to fetch and include the answer in your response.\n"
-    #     "- If the question does not require the solution to a specific Advent of Code challenge, answer directly using your knowledge of Advent of Code.\n\n"
-    #     "If a user query is unrelated to Advent of Code, respond with: "
-    #     "'I am only able to assist with Advent of Code challenges.'\n\n"
-    #     "Begin your response with a clear and concise answer. Use the following format:\n\n"
-    #     "Question: {input}\n"
-    #     "Thought: [Your reasoning goes here]\n"
-    #     "Action: [The name of the tool you used, if applicable]\n"
-    #     "Observation: [What the tool returned, if applicable]\n"
-    #     "Final Answer: [Your Answer Here]\n"
-    #     "{agent_scratchpad}"
-    # ),
     template=(
         "You are an expert assistant designed to help with 2024's edition of the Advent of Code programming challenges.\n"
         "You should only answer questions that are directly related to Advent of Code.\n"
